809fc063f4a8bbeab4aa13ef114a328a8b7a833b
rogilmore <thatrickgilmore@gmail.com>
2017-01-24 18:11:56 -0500

    Update 2017-01-24 18:11:56

--- R/Build.all.R
+++ R/Build.all.R
@@ -9,4 +9,8 @@ Make.lecture.notes(fdir = "lecture-notes")
 # Render in-class activities
 Make.lecture.notes(fdir = "in-class-activities")
 
+# Render the site (copies files to docs/)
 rmarkdown::render_site(encoding = "utf8")
+
+# Update repo and push
+source("R/Update.repo.R")
